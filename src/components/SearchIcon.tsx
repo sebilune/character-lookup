f904687ec3edd9a884d30a437b8adc7359935dd3
Sebi <118302324+sebilune@users.noreply.github.com>
2024-06-17 21:10:49 -0400

    refactor: separate SearchIcon SVG into component
    - Extract the SVG code for the SearchIcon into a standalone React component. This improves code readability and reusability.

---
+++ src/components/SearchIcon.tsx
@@ -0,0 +1,28 @@
+const SearchIcon: React.FC = () => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g stroke-width="0"></g>
+    <g stroke-linecap="round" stroke-linejoin="round"></g>
+    <g>
+      {" "}
+      <path
+        opacity="0.1"
+        d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        fill="currentColor"
+      ></path>{" "}
+      <path
+        d="M15 15L21 21"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>{" "}
+      <path
+        d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        stroke="currentColor"
+        stroke-width="2"
+      ></path>{" "}
+    </g>
+  </svg>
+);
+
+export default SearchIcon;
