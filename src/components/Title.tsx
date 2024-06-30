df0d5d1f8f306dd80f65716e71e091121d60c6d8
Sebi <sebi@sebilune.com>
2024-06-29 22:59:13 -0500

    refactor(icons): Remove custom search icon component, use react-icons instead
    - Remove the search icon SVG file and its references.
    - Install react-icons package.
    - Replace custom search icon component with an icon from react-icons.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,11 @@
-import SearchIcon from "./SearchIcon";
+import { MdOutlineManageSearch } from "react-icons/md";
 
 const Title: React.FC = () => (
   <h1 className="title">
     <a href="https://github.com/sebilune/character-lookup">
       Anime Character Lookup
     </a>
-    <SearchIcon />
+    <MdOutlineManageSearch className="search-icon" />
   </h1>
 );
 
